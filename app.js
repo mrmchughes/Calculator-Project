54b91946c920a7b60b5c92b20dc7febf9209bef2
mrmchughes <mrmchughes27@gmail.com>
2021-07-26 11:05:26 -0700

    logic updates, equal button works as intended

--- app.js
+++ app.js
@@ -1,97 +1,105 @@
 const display = document.querySelector('#calculatorDisplay');
-const operatorButtons = document.querySelectorAll('.operatorButtons').value;
+const operatorButtons = document.querySelectorAll('.operatorButtons');
 const clearButton = document.querySelector('#clearButton');
+const equalButton = document.querySelector('#equalButton');
 const numButtons = document.querySelectorAll('.numButtons');
-let result;
-let num1 = 7;
-let num2 = 7;
-let operator = '+';
 
-//On load, call clearScreen(), so display.div = 0,
+let total;
+//variable to hold actual total for operations?
+let num1 = 8; //operand 1
+let num2 = 7; //operand 2
+let operator;
+
+//On load, calls clearScreen(), so display.div = 0,
 window.addEventListener('load', clearScreen);
 
 let operationInProgress = false;
 //when = button is clicked, or a new operator is selected, set operationInProgress back to false//
 
-function add(num1, num2) {
-    result = num1 + num2;
-
-    return result;
+//adds num1 and num2, returns the total, sets display to the total
+const add = function(num1, num2) {
+    let total = num1 + num2;
+    display.innerHTML = total;
+    return total;
 }
 
-function subtract(num1, num2) {
-    result = num1 - num2;
-
-    return result;
+//subtracts num1 and num2, returns the total, sets display to the total
+const subtract = function(num1, num2) {
+    let total = num1 - num2;
+    display.innerHTML = total;
+    return total;
 }
 
-function multiply(num1, num2) {
-    result = num1 * num2;
-
-    return result;
+//multiplies num1 and num2, returns the total, sets display to the total
+const multiply = function(num1, num2) {
+    let total = num1 * num2;
+    display.innerHTML = total;
+    return total;
 }
 
-function divide(num1, num2) {
-    result = num1 / num2;
-
-    return result;
+//divides num1 and num2, returns the total, sets display to the total
+const divide = function(num1, num2) {
+    let total = num1 / num2;
+    display.innerHTML = total;
+    return total;
 }
 
-//sets result to a blank string, and display.innerHTML = 0
-//doing this allows me to have 0 in the display on load, but avoid having 0 added to the value of result
+//sets total to be an empty string, and display.innerHTML = 0
+//doing this allows me to have 0 in the display on load, but avoid having 0 added to the value of total in the display
 function clearScreen(){
-    result = '';
+    total = '';
     display.innerHTML = 0;
 }
 
 //clears the display, by calling clearScreen()
 clearButton.addEventListener('click', clearScreen);
 
-//operatorButton.forEach(button => {
-    //button.addEventListener('click', function() {
-        //operate(operatorButton.value)
-    //})
-//});
-
-
-//when clicked, a numButton will += itself to the result string, which starts as an empty string
-//once result has had the numButton value added, display.innerHTML is now set to be equal to result
-numButtons.forEach(button => {
-    button.addEventListener('click', function(){
-        result += button.value;
-        display.innerHTML = result; 
+//when an operator button is clicked, sets operator variable = to button.value, sets display.innerHTML to equal operator 
+operatorButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        operator = button.value;
+        display.innerHTML = button.value;
     })
 });
 
+//when clicked, total is set to button.value, which starts as an empty string
+//display.innerHTML is also set to equal the current value of total
+numButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        total = button.value;
+        display.innerHTML = total; 
+    })
+});
 
-
-//function setOperator(){
-    //
-//};
+//runs operate when clicked
+//currently works great, uses the value of num1, num2, and the operator as expected 
+//need to work on num 1 and 2 logic for multiple operations in a row
+equalButton.addEventListener('click', function(){
+    operate(num1, operator, num2);
+})
 
 //will need to configure operate() result to allow for multiple operations
 //+=, -=, *=, /=, will likely be used
 //will need to also allow for clearing result
 
-function operate(){ 
+function operate(num1, operator, num2){ 
 
     switch (operator){
-        case '+':
-            result = add(num1, num2);
+        case "+":
+            total = add(num1, num2);
             
             break;
-        case '-':
-            result = subtract(num1, num2);
+        case "-":
+            total = subtract(num1, num2);
             
             break;
-        case '*':
-            result = multiply(num1, num2);
+        case "*":
+            total = multiply(num1, num2);
 
             break;
-        case '/':
-            result = divide(num1, num2);
+        case "/":
+            total = divide(num1, num2);
 
             break;
     }
-    display.innerHTML = result;
 };
